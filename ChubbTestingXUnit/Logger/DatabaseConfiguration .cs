ea2842d6923b6ad1c564ad82912a4ccf0ebe6961
armanysys <armando.antoniodelangel@hotmail.com>
2024-10-24 10:06:11 -0600

    Added Mock Options

---
+++ ChubbTestingXUnit/Logger/DatabaseConfiguration .cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChubbTestingXUnit.Logger
+{
+    public class DatabaseConfiguration
+    {
+        public string ConnectionString { get; set; }
+    }
+    public class CustomerService
+    {
+        private readonly DatabaseConfiguration _configuration;
+        public CustomerService(IOptions<DatabaseConfiguration> configuration)
+        {
+            _configuration = configuration.Value;
+        }
+        public string GetConnectionString() => _configuration.ConnectionString;
+    }
+}
---
+++ XUnitTesting/DatabaseConnectionShould.cs
@@ -0,0 +1,65 @@
+﻿using ChubbTestingXUnit.Logger;
+using Microsoft.Extensions.Options;
+using Moq;
+
+namespace XUnitTesting
+{
+    public class DatabaseConnectionShould
+    {
+        private readonly DatabaseConfiguration _configData = new DatabaseConfiguration
+        {
+            ConnectionString = "TestConnectionString"
+        };
+        // This attribute marks the method as a test method in xUnit.
+        [Fact]
+        public void WhenMockingIOptionsWithMoq_ThenCorrectConnectionStringIsReturned()
+        {
+            // Create a mock object for IOptions<DatabaseConfiguration> using Moq.
+            var configMock = new Mock<IOptions<DatabaseConfiguration>>();
+            // Set up the mock to return _configData when the Value property is accessed.
+            configMock.Setup(x => x.Value).Returns(_configData);
+
+            // Create an instance of CustomerService, passing the mock object.
+            var sut = new CustomerService(configMock.Object);
+            // Call the GetConnectionString method on the CustomerService instance.
+            var result = sut.GetConnectionString();
+
+            // Assert that the result is equal to "TestConnectionString".
+            Assert.Equal("TestConnectionString", result);
+        }
+
+        // This attribute marks the method as a test method in xUnit.
+        [Fact]
+        public void WhenMockingIOptionsWithOptionsWrapper_ThenCorrectConnectionStringIsReturned()
+        {
+            // Create an OptionsWrapper object for DatabaseConfiguration with _configData.
+            var configWrapper = new OptionsWrapper<DatabaseConfiguration>(_configData);
+
+            // Create an instance of CustomerService, passing the OptionsWrapper object.
+            var sut = new CustomerService(configWrapper);
+            // Call the GetConnectionString method on the CustomerService instance.
+            var result = sut.GetConnectionString();
+
+            // Assert that the result is equal to "TestConnectionString".
+            Assert.Equal("TestConnectionString", result);
+        }
+
+        // This attribute marks the method as a test method in xUnit.
+        [Fact]
+        public void WhenMockingIOptionsWithOptionsHelper_ThenCorrectConnectionStringIsReturned()
+        {
+            // Create an IOptions object for DatabaseConfiguration using Options.Create helper method.
+            var configWrapperUsingHelper = Options.Create(_configData);
+
+            // Create an instance of CustomerService, passing the IOptions object.
+            var sut = new CustomerService(configWrapperUsingHelper);
+            // Call the GetConnectionString method on the CustomerService instance.
+            var result = sut.GetConnectionString();
+
+            // Assert that the result is equal to "TestConnectionString".
+            Assert.Equal("TestConnectionString", result);
+        }
+
+    }
+}
+
